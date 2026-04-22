b4ef55fccc952ff84db5288f326b4d49084628a3
Shekhar373 <shekhar24102004@gmail.com>
2026-04-22 18:58:05 +0530

    cursor hide in mobile

--- src/components/common/Cursor.jsx
+++ src/components/common/Cursor.jsx
@@ -1,34 +1,38 @@
-import React, { useEffect, useRef } from 'react'
-import gsap from 'gsap'
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const Cursor = () => {
   const cursorRef = useRef(null);
 
   useEffect(() => {
-    const mouseMoveHandler = (e) => {
-      gsap.to(cursorRef.current, {
-        x: e.clientX - 25,
-        y: e.clientY - 25,
-        duration: 0.5,
-        // ease: "expo.out",
-        ease:"power4.out"
-      });
-    };
+    const mm = gsap.matchMedia();
+    mm.add("(min-width: 1024px)", () => {
+      const mouseMoveHandler = (e) => {
+        gsap.to(cursorRef.current, {
+          x: e.clientX - 25,
+          y: e.clientY - 25,
+          duration: 0.5,
+          // ease: "expo.out",
+          ease: "power4.out",
+        });
+      };
 
-    window.addEventListener('mousemove', mouseMoveHandler);
+      window.addEventListener("mousemove", mouseMoveHandler);
 
-    return () => {
-      window.removeEventListener('mousemove', mouseMoveHandler);
-    };
+      return () => {
+        window.removeEventListener("mousemove", mouseMoveHandler);
+      };
+    });
+    return () => mm.revert();
   }, []);
 
   return (
     <div
       ref={cursorRef}
-      className='fixed pointer-events-none z-50 h-15 w-15 rounded-full border border-zinc-500 mix-blend-difference'
-      style={{left: 0, top: 0}}
+      className="fixed max-md:hidden pointer-events-none z-50 h-15 w-15 rounded-full border border-zinc-500"
+      style={{ left: 0, top: 0 }}
     ></div>
-  )
-}
+  );
+};
 
-export default Cursor
+export default Cursor;
